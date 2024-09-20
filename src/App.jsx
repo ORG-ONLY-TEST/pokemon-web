b78d1efed4db7157d9dcbe5b4e5c147b19974635
Willy Hardian <hardian.will@gmail.com>
2024-09-20 20:52:28 +0700

    feat: add image and give decoration to pokemon card in pokemon list

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ function App() {
         <>
             <h1>Pokemon</h1>
             <p>All about pokemon data</p>
-            <div>
+            <div class="bg-base-100">
+                <img src="" alt="" />
                 <h1>Bulbasur</h1>
                 <p>Bulbasur is green</p>
-                <button>Delete</button>
+                <button class="btn bg-red-200">Delete</button>
             </div>
-            <div>
+            <div class="bg-base-100">
+                <img src="" alt="" />
                 <h1>Squirtle</h1>
                 <p>Squirtle is blue</p>
-                <button>Delete</button>
+                <button class="btn bg-red-200">Delete</button>
             </div>
-            <div>
+            <div class="bg-base-100">
+                <img src="" alt="" />
                 <h1>Charmander</h1>
                 <p>Charmander is red</p>
-                <button>Delete</button>
+                <button class="btn bg-red-200">Delete</button>
             </div>
         </>
     );
