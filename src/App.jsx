37e53c77adcd6cbaf4daf1c735f465892d008298
Willy Hardian <hardian.will@gmail.com>
2024-09-20 20:24:53 +0700

    feat: give pokemon description

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ function App() {
             <p>All about pokemon data</p>
             <div>
                 <h1>Bulbasur</h1>
+                <p>Bulbasur is green</p>
             </div>
             <div>
                 <h1>Squirtle</h1>
+                <p>Squirtle is blue</p>
             </div>
             <div>
                 <h1>Charmander</h1>
+                <p>Charmander is red</p>
             </div>
         </>
     );
